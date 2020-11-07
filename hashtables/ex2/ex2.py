edc46d5db363dd49c5af53096eab994684565631
Hamza HArmouche <58759442+harmouche@users.noreply.github.com>
2020-11-06 23:54:17 -0500

    stretch

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,3 +1,4 @@
+
 #  Hint:  You may not need all of these.  Remove the unused functions.
 class Ticket:
     def __init__(self, source, destination):
@@ -10,5 +11,18 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    flights = {}
+    route = [None] * length
+
+    for i in range(0, length):
+        curr = tickets[i]
+        flights[curr.source] = curr.destination
+
+    route[0] = flights["NONE"]
+    route[1] = flights[route[0]]
+
+    if length > 2:
+        for i in range(2, length):
+            route[i] = flights[route[i-1]]
 
     return route
